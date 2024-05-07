7706350f4fc76144f4b300682a5a7659a9053438
Mashruf Ehsan <mashruf.ehsan@gmail.com>
2024-05-07 18:27:40 +0600

    Added google fonts

--- lib/answer_button.dart
+++ lib/answer_button.dart
@@ -8,14 +8,17 @@ class AnswerButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: onTap,
-      style: ElevatedButton.styleFrom(
-        padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 40,),
-        backgroundColor: const Color.fromARGB(255, 41, 20, 78),
-        foregroundColor: Colors.white,
+    return Container(
+      margin: const EdgeInsets.only(top: 10),
+      child: ElevatedButton(
+        onPressed: onTap,
+        style: ElevatedButton.styleFrom(
+          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 40,),
+          backgroundColor: const Color.fromARGB(255, 41, 20, 78),
+          foregroundColor: Colors.white,
+        ),
+        child: Text(answer, textAlign: TextAlign.center,),
       ),
-      child: Text(answer),
     );
   }
 }
